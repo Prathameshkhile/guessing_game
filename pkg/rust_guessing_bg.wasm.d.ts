48aef92a52ccc55219ea664ce73522b86c6262ef
Prathamesh Khile <pvkhile@gmail.com>
2025-07-30 15:13:43 +0000

    Updates

---
+++ pkg/rust_guessing_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_game_free: (a: number, b: number) => void;
+export const game_new: () => number;
+export const game_check_guess: (a: number, b: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
