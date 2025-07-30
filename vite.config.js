707050ef15862a5e1c43eeb7c9670bb56ba120d1
Prathameshkhile <123576094+Prathameshkhile@users.noreply.github.com>
2025-07-30 17:57:45 +0530

    frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/guessing_game/", // 👈 replace with repo name
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/pkg",
+          dest: "", // copy `pkg/` folder to dist root
+        },
+      ],
+    }),
+  ],
+});
